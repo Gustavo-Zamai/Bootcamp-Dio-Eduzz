9e901296f5e2ae0c556786442bf6f87b047705b9
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2021-12-26 00:22:45 -0300

    Aula 04
    Primeira parte da aula 04 onde foi abordado o conceito de Callback e Pormises.

---
+++ FuncoesES6/mod04.js
@@ -0,0 +1,11 @@
+//Callbacks and Promises
+//PROMISES
+Promise = new Promise (resolved, reject)
+//Pending == Processando
+//Fullfilled == Processado com sucesso
+//Rejected == Processado com algum erro
+
+
+
+
+
