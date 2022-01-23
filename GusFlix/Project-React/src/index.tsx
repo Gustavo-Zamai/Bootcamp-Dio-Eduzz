c1ac6ae6ee81e97fe86c4e348347cc532431c836
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-01-23 15:32:01 -0300

    Finalization of react project
    Create react app, to use github API and search users.

---
+++ GusFlix/Project-React/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Providers from "./providers";
+
+ReactDOM.render(
+  <>
+    <Providers />
+  </>,
+  document.getElementById("root")
+);
+
